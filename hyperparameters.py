96b9db52eb765f65ee748951ea553ad07ccf45e3
Ming <319407@qq.com>
2020-07-18 13:07:06 +0800

    Update hyperparameters.py

--- hyperparameters.py
+++ hyperparameters.py
@@ -11,7 +11,7 @@ import os
 import sys
 pwd = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(pwd)
-from classification_multi_label_01.utils import load_vocabulary
+from classification_multi_label_textcnn.utils import load_vocabulary
 
 
 
