2d78a4dbe3dfa58357b2e1d15b14004076e6597a
hellonlp <319407@qq.com>
2021-05-31 11:53:11 +0800

    Update classifier_utils.py

--- classifier_utils.py
+++ classifier_utils.py
@@ -267,16 +267,6 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
   else:
     label_id = example.label
 
-#  if ex_index < 5:
-#    tf.logging.info("*** Example ***")
-#    tf.logging.info("guid: %s" % (example.guid))
-#    tf.logging.info("tokens: %s" % " ".join(
-#        [tokenization.printable_text(x) for x in tokens]))
-#    tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-#    tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
-#    tf.logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-#    tf.logging.info("label: %s (id = %d)" % (example.label, label_id))
-
   feature = InputFeatures(
       input_ids=input_ids,
       input_mask=input_mask,
