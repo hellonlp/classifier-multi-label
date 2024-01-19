0386ae8dca51920833cc015347d6ec297b556463
陈明 <319407@qq.com>
2024-01-19 17:20:13 +0800

    Create eval.py

---
+++ classifier_multi_label_denses/eval.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 10:56:43 2021
+
+@author: cm
+"""
+
+
+
+# 参考： https://github.com/hellonlp/classifier-multi-label/blob/master/evaluation/eval_multi_label.py
